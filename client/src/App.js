10934e28f7d319203a44b9575a78a074221d03ab
YuJin <dldbwls3566@gmail.com>
2023-11-02 14:46:38 +0900

    server 완료 client react 설치

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/loginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
